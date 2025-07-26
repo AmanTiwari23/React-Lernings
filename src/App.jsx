7617dbd1084fe2a1813bbdbc66813e57204f6b27
Aman Tiwari <tiwari95aman@gmail.com>
2025-07-26 19:22:41 +0530

    useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import College, { Course, Fees } from "./assets/Cybrom";
 
@@ -8,17 +8,29 @@ const App = () => {
   const [name,setName] = useState("Tushar!");
   const [color,setColor] = useState("red");
 
+  const [count,setCount] = useState(0);
+
+  useEffect(()=>{
+    setTimeout(()=>{
+   setCount(count+1);
+    },3000)
+  },[])
+
+  const decrement=()=>{
+    if(count<=1){
+      alert("no decrement")
+    }else{
+      setCount(count-1);
+    }
+  }
   const myname = ()=>{
     setName("Aman Tiwari");
   }
   return (
     <>
-     <h1 style={{color:color}}>My favrouit color is : {color}</h1>
-     <button onClick={()=>{setColor("blue")}}>blue</button>
-     <button onClick={()=>{setColor("green")}}>green</button>
-     <button onClick={()=>{setColor("yellow")}}>yellow</button>
-     <button onClick={()=>{setColor("pink")}}>pink</button>
-     <button onClick={()=>{setColor("orange")}}>orange</button>
+     <button onClick={()=>{setCount(count+1)}}>Increment</button>
+     <h1>{count}</h1>
+     <button onClick={()=>{decrement()}}>decrement</button>
     </>
   );
 };
