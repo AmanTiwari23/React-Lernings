ea0ba7a1b7f15e140eba8db12f10b4e11156d910
Aman Tiwari <tiwari95aman@gmail.com>
2025-08-04 22:45:21 +0530

    added update and delet functionality

--- src/pages/Update.jsx
+++ src/pages/Update.jsx
@@ -1,9 +1,139 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 const Update = () => {
-  return (
-    <div>Update</div>
-  )
-}
+  const [myData, setMyData] = useState([]);
 
-export default Update
+  const [editId, setEditId] = useState(null);
+
+  const [formData,setFormData] = useState({
+    roll:"",
+    name:"",
+    city:"",
+    fees:""
+  });
+
+  const loadData = async () => {
+    const api = "http://localhost:3000/students";
+    const response = await axios.get(api);
+    console.log(response.data);
+    setMyData(response.data);
+  };
+
+  const myDel = async (id) => {
+    let api = `http://localhost:3000/students/${id}`;
+    const response = await axios.delete(api);
+    console.log(response);
+    alert("Record Deleted !!");
+    loadData();
+  };
+
+  const myUpdate = (item) => {
+    setEditId(item.id);
+    setFormData({
+      roll: item.roll,
+      name: item.name,
+      city: item.city,
+      fees: item.fees
+    });
+    
+  };
+
+  const handleChange=(e)=>{
+    setFormData(prev=>({
+      ...prev,[e.target.name]:e.target.value
+    }));
+  }
+
+  const handleSubmit = async (e) =>{
+    e.preventDefault();
+  }
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  const ans = myData.map((item) => {
+    return (
+      <>
+        <tr>
+          <td>{item.roll}</td>
+          <td>{item.name}</td>
+          <td>{item.city}</td>
+          <td>{item.fees}</td>
+          <td>
+            <button
+              onClick={() => {
+                myDel(item.id);
+              }}
+            >
+              Delete
+            </button>
+          </td>
+          <td>
+            <button
+              onClick={() => {
+                myUpdate(item);
+              }}
+            >
+              Edit
+            </button>
+          </td>
+        </tr>
+      </>
+    );
+  });
+
+  return (
+    <>
+      <div>Update Page</div>
+
+      <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+        <input
+          type="text"
+          name="roll"
+          placeholder="Roll No"
+          value={formData.roll}
+        />
+
+         <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+        />
+
+         <input
+          type="text"
+          name="city"
+          placeholder="City"
+          value={formData.city}
+        />
+
+         <input
+          type="text"
+          name="fees"
+          placeholder="Fees"
+          value={formData.fees}
+        />
+
+        <button type="submit">
+          {editId === null ? "Select Row to Edit" : "Update Record"}
+        </button>
+
+      </form>
+      <table border="1" width="700">
+        <tr>
+          <th>Roll no</th>
+          <th>Name</th>
+          <th>City</th>
+          <th>Fees</th>
+          <th></th>
+          <th></th>
+        </tr>
+        {ans}
+      </table>
+    </>
+  );
+};
+
+export default Update;
