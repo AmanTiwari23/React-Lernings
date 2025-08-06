6391608dd1b4a6680a6133df0a3dfd953cfd8a7a
Aman Tiwari <tiwari95aman@gmail.com>
2025-08-06 23:21:29 +0530

    developing seprate edit page

--- src/pages/MyEdit.jsx
+++ src/pages/MyEdit.jsx
@@ -4,9 +4,55 @@ import { useParams } from 'react-router-dom';
 const MyEdit = () => {
 
     const {id}=useParams();
+    const [editId, setEditId] = useState(null);
+    
+      const [formData,setFormData] = useState({
+        roll:"",
+        name:"",
+        city:"",
+        fees:""
+      });
   return (
     <>
     <h1>Edit Record : {id}</h1>
+    <form onSubmit={handleSubmit} style={{ marginBottom: "20px" }}>
+        <input
+          type="text"
+          name="roll"
+          placeholder="Roll No"
+          value={formData.roll}
+          onChange={handleChange}
+        />
+
+         <input
+          type="text"
+          name="name"
+          placeholder="Name"
+          value={formData.name}
+          onChange={handleChange}
+        />
+
+         <input
+          type="text"
+          name="city"
+          placeholder="City"
+          value={formData.city}
+          onChange={handleChange}
+        />
+
+         <input
+          type="text"
+          name="fees"
+          placeholder="Fees"
+          value={formData.fees}
+          onChange={handleChange}
+        />
+
+        <button type="submit">
+          {editId === null ? "Select Row to Edit" : "Update Record"}
+        </button>
+
+      </form>
     </>
   )
 }
