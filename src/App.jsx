5e0d4875daa0b9f20a7ab13d0b935121d00b5f00
Aman Tiwari <tiwari95aman@gmail.com>
2025-09-02 17:44:32 +0530

    redux lernings

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,18 @@
-// import { BrowserRouter, Route , Routes } from "react-router-dom";
-// import Layout from "./Layout";
-// import Update from "./pages/Update";
-// import Insert from "./pages/Insert";
-// import Home from "./pages/Home";
-// import About from "./pages/About";
-// import Contact from "./pages/Contact";
-// import Display from "./pages/Display";
-// import MyEdit from "./pages/MyEdit";
-// import Search from "./pages/Search";
-
+import {useSelector,useDispatch} from "react-router";
+import {colorChange} from "./colorSlice";
 import { useState } from "react";
-import Comp1 from "../prop/Comp1";
-
 
 const App = ()=>{
-
-    const [user,setUser] = useState("Aman");
+  const myclr = useSelector(state => state.mycolor.color);
+  const dispatch = useDispatch();
+  const [txtval, setTxtVal] = useState("");
+    
     return(
         <>
-        {/* <BrowserRouter>
-        <Routes>
-            <Route path="/" element = {<Layout/>}>
-             <Route index element={<Home/>}/>
-             <Route path="about" element={<About/>}/>
-             <Route path="home" element={<Home/>}/>
-             <Route path="contact" element={<Contact/>}/>
-             <Route path="update" element={<Update/>}/>
-             <Route path="insert" element={<Insert/>}/>
-             <Route path="display" element={<Display/>}/>
-             <Route path="myedit/:id" element={<MyEdit/>}/>
-             <Route path="search" element={<Search/>}/>
-
-            </Route>
-        </Routes>
-
-        </BrowserRouter> */}
-        <h1>Welcome to Cybrom : {user}</h1>
-        <Comp1 user={user}/>
+        <h1>welcome</h1>
+        Enter color: <input type="text" onChange={(e)=>{setTxtVal(e.target.value)}}/>
+        <button onClick={()=>{dispatch(colorChange(txtval))}}>Click here !</button>
+        <div style={{width:"400px", height:"200px", backgroundColor:myclr}}></div>
         </>
     )
 }
